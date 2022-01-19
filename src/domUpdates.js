28e91c96971f9e18b2b54267e0b62f09bbe707dd
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-01-18 23:03:53 -0500

    Complete iteration 3

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,8 @@
 const domUpdates = {
   renderGreeting(currentCustomer,hotel) {
     return `<div class='welcome-container' id='welcomeContainer'>
-      <h1 class='greeting' id='greeting'> Welcome back, ${currentCustomer.name}!</h1>
-      <h2 class='total-spent' id='totalSpent'>You have spent $${hotel.findCustomerTotal(currentCustomer.id)} on trips this year.</h2>
+      <h3 class='greeting' id='greeting'> Welcome back, ${currentCustomer.name}!</h3>
+      <h3 class='total-spent' id='totalSpent'>You have spent $${hotel.findCustomerTotal(currentCustomer.id)} on trips this year.</h3>
     </div>`
   },
   renderBooking(booking) {
