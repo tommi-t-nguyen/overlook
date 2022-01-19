7795b63f1469c6820e6c732fa270a769bf3f6f69
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-01-18 20:07:54 -0500

    Complete iteration 2 functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,18 @@
 import './css/base.scss';
-import { fetchData, getUser } from './fetch';
+import { fetchData, getUser, bookRoomPost } from './fetch';
 import Hotel from '../src/classes/Hotel';
 import domUpdates from './domUpdates'
 
 // global varibles
-let currentCustomer, hotel, currentCustomerBookings
+let currentCustomer, hotel, currentCustomerBookings, roomsAvailable, date
 
 // query selector
 const welcomeContainer = document.querySelector('#welcomeContainer')
 const bookingContainer = document.querySelector('#bookingsBoard')
+const bookingPage = document.querySelector('#bookingPage')
+const showRoomsAvailable = document.querySelector('#showRoomsAvailable')
+const bookingMessage = document.querySelector('#bookingMessage')
+const bookRoom = document.querySelector('#bookRoom')
 // functions
 const onStart = (id) => {
   return Promise.all([getUser(id), fetchData('rooms'), fetchData('bookings')])
@@ -18,17 +22,60 @@ const onStart = (id) => {
 const loadPage = (data) => {
   currentCustomer = data[0];
   hotel = new Hotel(data[1].rooms, data[2].bookings);
-  welcomeContainer.innerHTML = domUpdates.renderGreeting(currentCustomer,hotel);
+  welcomeContainer.innerHTML = domUpdates.renderGreeting(currentCustomer, hotel);
   currentCustomerBookings = hotel.findCustomerBooking(currentCustomer.id);
+  console.log(currentCustomerBookings);
   loadBookings(currentCustomerBookings);
 }
 
 const loadBookings = (currentCustomerBookings) => {
   bookingContainer.innerHTML = '';
-  currentCustomerBookings.forEach(booking =>{
+  currentCustomerBookings.forEach(booking => {
     bookingContainer.innerHTML += domUpdates.renderBooking(booking)
   })
 }
 
+const filterRoom = (date, roomType) => {
+  let filterRoom = roomsAvailable.filter(room => room.roomType == roomType)
+  if (roomType == "") {
+    roomsAvailable.forEach(room => {
+      bookingMessage.innerHTML += domUpdates.renderAvailableRooms(room)
+    })
+  }else if (filterRoom.length == 0){
+    bookingMessage.innerHTML += `<p>Sorry no rooms available. Please refined your search!</p>`
+  }
+   else {
+    filterRoom.forEach(room => {
+      bookingMessage.innerHTML += domUpdates.renderAvailableRooms(room)
+    })
+  }
+}
+
+const bookARoom = (e) => {
+  if(e.target.closest('button')){
+  const roomToBeBook = {
+    userID: currentCustomer.id,
+    date: date,
+    roomNumber: Number(e.target.closest('button').id)
+  }
+  bookRoomPost(roomToBeBook)
+  .then(response => onStart(5))
+}
+}
+
+
 // event listeners
 window.addEventListener('load', onStart(5));
+bookingMessage.addEventListener('click',  (e) => {
+  bookARoom(e)
+});
+
+showRoomsAvailable.addEventListener('submit', (e) => {
+  e.preventDefault();
+  bookingMessage.innerHTML = '';
+  const formData = new FormData(e.target);
+  date = formData.get('date').replace(/-/gi, "/");
+  let roomType = formData.get('roomType')
+  roomsAvailable = hotel.roomsAvailable(date)
+  filterRoom(date, roomType);
+});
